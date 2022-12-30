ff91544e91591c02f612cd38d05c2730869201f3
Fersat <fertagram@gmail.com>
2022-12-30 13:46:31 +0300

    fix(useelementsize): "window is not defined"

--- src/useElementSize/index.ts
+++ src/useElementSize/index.ts
@@ -1,48 +1,46 @@
 // https://usehooks-ts.com/react-hook/use-element-size
-import { useCallback, useState } from 'react'
-// See: https://usehooks-ts.com/react-hook/use-event-listener
-import useEventListener from '../useEventListener'
+import { useCallback, useState } from 'react';
 // See: https://usehooks-ts.com/react-hook/use-isomorphic-layout-effect
-import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect'
+import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect';
+import { useWindowEvent } from '../useWindowEvent';
 
 interface Size {
-  width: number
-  height: number
+   height: number;
+   width: number;
 }
 
-function useElementSize<T extends HTMLElement = HTMLDivElement>(): [
-  (node: T | null) => void,
-  Size
-] {
-  // Mutable values like 'ref.current' aren't valid dependencies
-  // because mutating them doesn't re-render the component.
-  // Instead, we use a state as a ref to be reactive.
-  const [ref, setRef] = useState<T | null>(null)
-  const [size, setSize] = useState<Size>({
-    width: 0,
-    height: 0
-  })
+/**
+ *
+ * @example[squareRef, { width, height }] = useElementSize<HTMLDivElement>();
+ * @returns [setRef, size]
+ */
+function useElementSize<T extends HTMLElement = HTMLDivElement>(): [(node: T | null) => void, Size] {
+   // Mutable values like 'ref.current' aren't valid dependencies
+   // because mutating them doesn't re-render the component.
+   // Instead, we use a state as a ref to be reactive.
+   const [ref, setRef] = useState<T | null>(null);
+   const [size, setSize] = useState<Size>({
+      width: 0,
+      height: 0
+   });
 
-  // Prevent too many rendering using useCallback
-  const handleSize = useCallback(() => {
-    setSize({
-      width: ref?.offsetWidth || 0,
-      height: ref?.offsetHeight || 0
-    })
+   // Prevent too many rendering using useCallback
+   const handleSize = useCallback(() => {
+      setSize({
+         width: ref?.offsetWidth || 0,
+         height: ref?.offsetHeight || 0
+      });
+   }, [ref?.offsetHeight, ref?.offsetWidth]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref?.offsetHeight, ref?.offsetWidth])
+   useWindowEvent('resize', handleSize, {
+      capture: true
+   });
 
-  useEventListener(window, 'resize', handleSize, {
-    capture: true
-  })
+   useIsomorphicLayoutEffect(() => {
+      handleSize();
+   }, [ref?.offsetHeight, ref?.offsetWidth]);
 
-  useIsomorphicLayoutEffect(() => {
-    handleSize()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref?.offsetHeight, ref?.offsetWidth])
-
-  return [setRef, size]
+   return [setRef, size];
 }
 
-export default useElementSize
+export default useElementSize;
