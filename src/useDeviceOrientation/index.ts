a2315c41227ce5e11735d02e864822eaed95b67d
Fersat <fertagram@gmail.com>
2022-12-30 13:47:12 +0300

    style(usedeviceorientation): prettier

--- src/useDeviceOrientation/index.ts
+++ src/useDeviceOrientation/index.ts
@@ -1,45 +1,39 @@
 // https://github.com/21kb/react-hooks/blob/main/packages/react-device-orientation-hook/src/index.ts
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 export interface IOrientationState {
-  angle: number
-  type: string
+   angle: number;
+   type: string;
 }
 
 export const defaultState: IOrientationState = {
-  angle: 0,
-  type: 'landscape-primary'
-}
+   angle: 0,
+   type: 'landscape-primary'
+};
 
-const useDeviceOrientation = (
-  initialState: IOrientationState = defaultState
-) => {
-  const [state, setState] = useState(initialState)
+const useDeviceOrientation = (initialState: IOrientationState = defaultState) => {
+   const [state, setState] = useState(initialState);
 
-  const onOrientationChangeEvent = () => {
-    const { orientation } = screen as any
-    const { angle, type } = orientation
+   const onOrientationChangeEvent = () => {
+      const { orientation } = screen;
+      const { angle, type } = orientation;
 
-    if (!orientation) {
-      setState(initialState)
-    }
+      if (!orientation) {
+         setState(initialState);
+      }
 
-    setState({ angle, type })
-  }
+      setState({ angle, type });
+   };
 
-  useEffect(() => {
-    window.addEventListener('orientationchange', onOrientationChangeEvent, true)
+   useEffect(() => {
+      window.addEventListener('orientationchange', onOrientationChangeEvent, true);
 
-    return () => {
-      window.addEventListener(
-        'orientationchange',
-        onOrientationChangeEvent,
-        true
-      )
-    }
-  }, [])
+      return () => {
+         window.addEventListener('orientationchange', onOrientationChangeEvent, true);
+      };
+   }, []);
 
-  return state
-}
+   return state;
+};
 
-export default useDeviceOrientation
+export { useDeviceOrientation };
