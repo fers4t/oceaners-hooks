f655bfcc2c8bdb9641ca3983c1bbb84578d937b9
Fersat <fertagram@gmail.com>
2022-12-31 03:20:11 +0300

    feat(usesafeeffect): added `useSafeEffect` hook

---
+++ src/misc/createSafeEffect.ts
@@ -0,0 +1,25 @@
+import { useRef } from 'react';
+import type { useEffect, useLayoutEffect } from 'react';
+
+type EffectHookType = typeof useEffect | typeof useLayoutEffect;
+
+const createSafeEffect: (hook: EffectHookType) => EffectHookType = (hook) => (effect, deps) => {
+   const isMounted = useRef(false);
+
+   // for react-refresh
+   hook(() => {
+      return () => {
+         isMounted.current = false;
+      };
+   }, []);
+
+   hook(() => {
+      if (!isMounted.current) {
+         isMounted.current = true;
+      } else {
+         return effect();
+      }
+   }, deps);
+};
+
+export { createSafeEffect };
---
+++ src/useSafeEffect/index.ts
@@ -0,0 +1,4 @@
+import { useEffect } from 'react';
+import { createSafeEffect } from '../misc/createSafeEffect';
+
+export default createSafeEffect(useEffect);
