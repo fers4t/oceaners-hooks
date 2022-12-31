453f0e3572a94c0afcb67d9cc5309f9474c4ded5
Fersat <fertagram@gmail.com>
2023-01-01 01:50:31 +0300

    chore: typos

--- src/useActive/index.tsx
+++ src/useActive/index.tsx
@@ -1,24 +1,23 @@
-// https://sandiiarov.github.io/use-events/#/docs-use-focus
-import React from 'react'
+import React from 'react';
 
 function useActive(): [
-  boolean,
-  {
-    onMouseDown: (e: React.MouseEvent) => void
-    onMouseUp: (e: React.MouseEvent) => void
-  }
+   boolean,
+   {
+      onMouseDown: (e: React.MouseEvent) => void;
+      onMouseUp: (e: React.MouseEvent) => void;
+   }
 ] {
-  const [isActive, setActive] = React.useState(false)
+   const [isActive, setActive] = React.useState(false);
 
-  const bind = React.useMemo(
-    () => ({
-      onMouseDown: () => void setActive(true),
-      onMouseUp: () => void setActive(false)
-    }),
-    []
-  )
+   const bind = React.useMemo(
+      () => ({
+         onMouseDown: () => void setActive(true),
+         onMouseUp: () => void setActive(false)
+      }),
+      []
+   );
 
-  return [isActive, bind]
+   return [isActive, bind];
 }
 
-export default useActive
+export default useActive;
