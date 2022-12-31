e16437ceed2b9b03a97a1ae98eee16c64265467b
Fersat <fertagram@gmail.com>
2022-12-31 04:20:11 +0300

    feat: added `useObjectState` hook

--- src/types/index.ts
+++ src/types/index.ts
@@ -31,3 +31,19 @@ export function getTargetElement<T extends TargetType>(target: BasicTarget<T>, d
 
    return targetElement;
 }
+
+/**
+ * Typed generic callback function, used mostly internally
+ * to defined callback setters
+ */
+export interface SomeCallback<TArgs, TResult = void> {
+   (...args: TArgs[]): TResult;
+}
+
+/**
+ * A callback setter is generally used to set the value of
+ * a callback that will be used to perform updates
+ */
+export interface CallbackSetter<TArgs> {
+   (nextCallback: SomeCallback<TArgs>): void;
+}
