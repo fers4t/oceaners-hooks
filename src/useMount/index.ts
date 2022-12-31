120885e1b82aaf3e6057e4d41f6f0dfb67efaf7e
Fersat <fertagram@gmail.com>
2022-12-31 03:05:23 +0300

    feat(usemount): added `useMount` hook

---
+++ src/useMount/index.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { isDev, isFunction } from '../misc';
+
+const useMount = (fn: () => void) => {
+   if (isDev) {
+      if (!isFunction(fn)) {
+         console.error(`useMount: parameter \`fn\` expected to be a function, but got "${typeof fn}".`);
+      }
+   }
+
+   useEffect(() => {
+      fn?.();
+   }, []);
+};
+
+export default useMount;
