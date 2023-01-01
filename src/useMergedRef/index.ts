e559a302dae967d36b183387d5d3cf2146d44b80
Fersat <fertagram@gmail.com>
2023-01-01 07:24:41 +0300

    perf: improvements for `useMergedRef`

--- src/useMergedRef/index.ts
+++ src/useMergedRef/index.ts
@@ -1,23 +1,33 @@
-import React, { useCallback } from 'react';
+import React, { useRef, useEffect } from 'react';
 
-type Ref<T> = React.Dispatch<React.SetStateAction<T>> | React.ForwardedRef<T>;
+type Ref<T> = React.MutableRefObject<T> | ((instance: T | null) => void);
 
-export function mergeRefs<T = any>(...refs: Ref<T>[]) {
-   return (node: T | null) => {
-      // @ts-ignore
-      refs.forEach((ref) => assignRef(ref, node));
-   };
+export function mergeRefs<T>(...refs: Ref<T>[]) {
+   const targetRef = useRef<T>(null);
+
+   useEffect(() => {
+      refs.forEach((ref) => {
+         if (!ref) return;
+
+         if (typeof ref === 'function') {
+            ref(targetRef.current);
+         } else {
+            ref.current = targetRef.current;
+         }
+      });
+   }, [refs]);
+
+   return targetRef;
 }
 
-export function useMergedRef<T = any>(...refs: Ref<T>[]) {
-   return useCallback(mergeRefs(...refs), refs);
+export function useMergedRef<T>(...refs: Ref<T>[]) {
+   return mergeRefs(...refs);
 }
 
-export function assignRef<T = any>(ref: React.ForwardedRef<T>, value: T | null) {
+export function assignRef<T>(ref: Ref<T>, value: T) {
    if (typeof ref === 'function') {
       ref(value);
-   } else if (typeof ref === 'object' && ref !== null && 'current' in ref) {
-      // eslint-disable-next-line no-param-reassign
+   } else if (ref) {
       ref.current = value;
    }
 }
