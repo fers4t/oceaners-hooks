84964f154c2350d6b5aea0280c509c3b5227ba17
Fersat <fertagram@gmail.com>
2023-01-01 11:55:24 +0300

    docs: `useValidatedState`

--- src/useValidatedState/index.ts
+++ src/useValidatedState/index.ts
@@ -6,15 +6,17 @@ export interface ValidationResult {
 }
 
 /**
- * Returns a state that changes only if the next value pass its validator
+ * Returns a state that changes only if the next value passes its validator
  */
 const useValidatedState = <TValue, TValidator extends (value: TValue) => boolean>(
    validator: TValidator,
    initialValue?: TValue
-) => {
+): [TValue, (nextValue: TValue) => void, ValidationResult] => {
    const [state, setState] = useState<TValue>(initialValue);
    const validation = useRef<ValidationResult>({ changed: false });
 
+   // We use useCallback here to prevent unnecessary re-renders.
+   // The dependency array ensures that the callback only changes if validator changes.
    const onChange = useCallback(
       (nextValue: TValue) => {
          setState(nextValue);
@@ -23,7 +25,7 @@ const useValidatedState = <TValue, TValidator extends (value: TValue) => boolean
       [validator]
    );
 
-   return [state, onChange, validation.current] as [TValue, (nextValue: TValue) => void, ValidationResult];
+   return [state, onChange, validation.current];
 };
 
 export default useValidatedState;
