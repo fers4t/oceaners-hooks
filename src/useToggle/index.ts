fb5037be2328cf2b08c4a65d01615ae0af737d7c
Fersat <fertagram@gmail.com>
2023-01-04 01:04:14 +0300

    build: trying to fix hook errors

--- src/useToggle/index.ts
+++ src/useToggle/index.ts
@@ -9,14 +9,25 @@ type Actions<T> = {
 
 function useToggle<T = boolean>(): [boolean, Actions<T>];
 function useToggle<T>(defaultValue: T): [T, Actions<T>];
-function useToggle<T, U>(defaultValue: T, reverseValue: U): [T | U, Actions<T | U>];
-function useToggle<D, R>(defaultValue: D = false as unknown as D, reverseValue?: R) {
+function useToggle<T, U>(
+   defaultValue: T,
+   reverseValue: U
+): [T | U, Actions<T | U>];
+function useToggle<D, R>(
+   defaultValue: D = false as unknown as D,
+   reverseValue?: R
+) {
    const [state, setState] = useState<D | R>(defaultValue);
 
    const actions = useMemo((): Actions<D | R> => {
-      const reverseValueOrigin = (reverseValue === undefined ? !defaultValue : reverseValue) as D | R;
+      const reverseValueOrigin = (
+         reverseValue === undefined ? !defaultValue : reverseValue
+      ) as D | R;
 
-      const toggle = () => setState((s) => (s === defaultValue ? reverseValueOrigin : defaultValue));
+      const toggle = () =>
+         setState((s) =>
+            s === defaultValue ? reverseValueOrigin : defaultValue
+         );
       const set = (value: D | R) => setState(value);
       const setLeft = () => setState(defaultValue);
       const setRight = () => setState(reverseValueOrigin);
